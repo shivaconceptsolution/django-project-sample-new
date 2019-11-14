1efcd14b3d73c70e898e10f43d37a57281510d18
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2019-11-14 04:41:23 -0800

    Add files via upload

---
+++ myapp/feedapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+	return render(request,"feedapp/index.html")
+def about(request):
+	return render(request,"feedapp/about.html")	
+def contact(request):
+	return render(request,"feedapp/contact.html")	
+def service(request):
+	return render(request,"feedapp/service.html")
+def gallery(request):
+	return render(request,"feedapp/gallery.html")
+
